1cbda47217ff9081bd1d79baa90a922ff76a3eb3
Nik Voss <niklas.voss@gmail.com@>
2016-11-04 16:59:51 +0100

    Fixed, tested and finished up integration of ag4 rest service and twitter metrics.

--- preprocess.py
+++ preprocess.py
@@ -10,13 +10,14 @@ NOTE: textblob needs language files, download them
 ===================================
 """
 
+import calendar
 import time
 import random
 import os
 import logging
 import numpy as np
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from langid import classify
 from pymongo import MongoClient, GEO2D, ASCENDING
 from textblob import TextBlob as TextBlobEN
@@ -29,11 +30,18 @@ logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', level=log
 # TODO: possibly others? see: http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 ALLOWED_WORD_TOKENS = { 'N', 'J', 'V' }
 
+# Custom exceptions
+class RestConnectionException(Exception):
+	pass
+class UnknownLanguageException(Exception):
+	pass
+
+# Connect to 
 def connect_to_and_setup_database():
 	while True:
 		try:
 			addr = os.getenv('MONGODB_PORT_27017_TCP_ADDR', 'localhost')
-			port = os.getenv('MONGODB_PORT_27017_TCP_PORT', '27017')
+			port = os.getenv('MONGODB_PORT_27017_TCP_PORT', '27018')
 			passwd = os.getenv('MONGODB_PASS', 'supertopsecret')
 			client = MongoClient('mongodb://analysis:' + passwd + '@' + addr + ':' + port + '/analysis')
 			db = client.analysis
@@ -44,33 +52,35 @@ def connect_to_and_setup_database():
 			logging.error(repr(error))
 			time.sleep(2) # wait with the retry, database is possibly starting up
 
+#
 def get_rest_get_via_timestamp_url():
 	addr = os.getenv('REST_PORT_3000_TCP_ADDR', 'localhost')
 	port = os.getenv('REST_PORT_3000_TCP_PORT', '3000')
 	return 'http://' + addr + ':' + port + '/tweets/ts/'
 
+#
 def get_new_tweets(url, newer_than_time):
-	res = requests.get(url + int(calendar.timegm(dt.utctimetuple()) * 1000))
+	res = requests.get(url + str(int(calendar.timegm(newer_than_time.utctimetuple()) * 1000)))
 	if res.status_code == 200:
 		tweets = res.json()
 		return tweets if type(tweets) is list else list(tweets)
 	else:
-		raise Exception('Connection to tweetdb rest-service failed with status-code ' + res.status_code + ': ' + res.text)
+		raise RestConnectionException('Connection to tweetdb rest-service failed with status-code ' + res.status_code + ': ' + res.text)
 
-# basic tweet class, takes the raw tweet data and already preprocesses for a better internal representation
+# takes the raw tweet data and already preprocesses (includes sentiment analysis) for a better internal representation 
 def preprocess_tweet(data):
 	try:
-		create_on = datetime.utcfromtimestamp(date.timestamp_ms/1000)
+		created_at = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(data['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
 		# detect the language of the tweet or use predefined language
-		lang = classify(data.text)[0] if not data.lang else data.lang
+		lang = classify(data['text'])[0] if not 'lang' in data else data['lang']
 		# tokenize the text dependent on the language
 		blob = None
 		if lang == 'en':
-			blob = TextBlobEN(data.text)
+			blob = TextBlobEN(data['text'])
 		elif lang == 'de':
-			blob = TextBlobDE(data.text)
+			blob = TextBlobDE(data['text'])
 		else: # avoid unknown languages
-			raise Exception('Unknown language: ' + data.text)
+			raise UnknownLanguageException('Unknown language: ' + data['text'])
 		# get the polarity of the tweet sentences and summerize them
 		# NOTE: TextBlobDE is not as great as the english analyzer and is fairly barebone.
 		#	    If the resulting polarity is inaccurate, one possibility to solve this is to
@@ -105,7 +115,7 @@ def preprocess_tweet(data):
 		# or center of place
 		# TODO: check if coordinates exist before using place
 		# TODO: verify structure of place coordinates
-		coords = data.place.bounding_box.coordinates[0] 
+		coords = data['place']['bounding_box']['coordinates'][0] 
 		loc = [0.0, 0.0]
 		for coord in coords:
 			loc[0] += coord[0]
@@ -113,31 +123,43 @@ def preprocess_tweet(data):
 		loc[0] /= len(coords)
 		loc[1] /= len(coords)
 		# create tweet object 
-		tweet = { "author": author,
-				  "created_on": create_on,
+		tweet = { "tweet_id": data['_id'],
+				  "user": {
+				  	"name": data['user']['name'],
+				  	"screen_name": data['user']['screen_name'],
+				  	"followers_count": data['user']['followers_count'],
+				  	"friends_count": data['user']['friends_count'],
+				  	"listed_count": data['user']['listed_count'],
+				  	"statuses_count": data['user']['statuses_count'],
+				  	"following": data['user']['following']
+				  },
+				  "created_at": created_at,
 				  "words": words,
 				  "loc": loc,
 				  "polarity": polarity,
-				  "retweet_count": data.retweet_count,
-				  "favorite_count": data.favorite_count }
+				  "retweet_count": data['retweet_count'],
+				  "favorite_count": data['favorite_count'] }
 		return tweet
-	except Exception as error: # catch exceptions, usually failed language detection
+	except UnknownLanguageException as error: # catch exceptions, usually failed language detection
 		logging.warning(repr(error))
 
 if __name__ == '__main__':
-	# connect to mongodb
+	# connect to mongodb, get rest url and setup last_access_time
 	client, db = connect_to_and_setup_database()
 	url = get_rest_get_via_timestamp_url()
-	last_access_time = datetime.utcnow() - 60*60*24
-	while True:
+	last_access_time = datetime.utcnow() - timedelta(days=1)
+	# always try to get new tweets and process them
+	while True: 
 		try:
 			raw_tweets = get_new_tweets(url, last_access_time)
-			last_access_time = datetime.utcnow()
-			processed_tweets = list(map(preprocess_tweet, raw_tweets))
-			# insert data into mongo
-			result = db.tweets.insert_many(data)
-			logging.debug("Inserted ids: %s", result.inserted_ids)
-		except Exception as error:
+			if len(raw_tweets) > 0:
+				last_access_time = datetime.utcnow()
+				processed_tweets = list(filter(None.__ne__, map(preprocess_tweet, raw_tweets)))
+				# insert data into mongo
+				result = db.tweets.insert_many(processed_tweets)
+				logging.debug("Inserted ids: %s", result.inserted_ids)
+			time.sleep(1)
+		except RestConnectionException as error:
 			logging.warning(repr(error))
 
 
